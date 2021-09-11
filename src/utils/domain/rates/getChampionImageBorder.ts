15027cbdc1f833f5518845b2fb57cf85b8fa66d8
Paulo Endoh <paulo.endoh@gmail.com>
2021-09-11 14:22:10 -0300

    Added khaki color for 50~51 win rate

--- src/utils/domain/rates/getChampionImageBorder.ts
+++ src/utils/domain/rates/getChampionImageBorder.ts
@@ -3,6 +3,7 @@ import theme from "../../theme";
 
 export const getChampionImageBorder = (avgWin: number) => {
   if (avgWin >= 51) return `2px solid ${theme.palette.primary.main}`;
-  if (avgWin >= 49) return `2px solid ${myColors.ratingYellow[5]}`;
+  if (avgWin >= 50) return `2px solid khaki`;
+  if (avgWin >= 49) return `2px solid orange`;
   return `2px solid ${theme.palette.error.main}`;
 }
