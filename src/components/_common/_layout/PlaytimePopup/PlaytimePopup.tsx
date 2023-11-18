085e18ad93368cf669f4657cbf81c957dc8e854a
Paulo Endoh <paulo.endoh@gmail.com>
2023-11-18 04:09:09 -0300

    personal time limit -- modal + color [clp3o4h870003wk0ksskjjt13]

--- src/components/_common/_layout/PlaytimePopup/PlaytimePopup.tsx
+++ src/components/_common/_layout/PlaytimePopup/PlaytimePopup.tsx
@@ -1,11 +1,20 @@
-import { CgExternal } from "react-icons/cg";
-
 import { usePlaytimeQuery } from "@/hooks/react-query/domain/rates/usePlaytimeQuery";
 import useAramHelperStore from "@/hooks/zustand-stores/domain/aram-helper/useAramHelperStore";
 import { localStorageKeys } from "@/utils/consts/localStorageKeys";
-import { Box, CircularProgress, Typography } from "@material-ui/core";
-import { useEffect, useMemo } from "react";
+import { useLocalStorage } from "@mantine/hooks";
+import {
+  Box,
+  CircularProgress,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Divider,
+  Typography,
+} from "@material-ui/core";
+import { useEffect, useMemo, useState } from "react";
 import FlexCol from "../../flexboxes/FlexCol";
+import FlexVCenter from "../../flexboxes/FlexVCenter";
+import MyTextField from "../../inputs/MyTextField";
 
 type Props = {};
 
@@ -21,55 +30,191 @@ const PlaytimePopup = ({ ...props }: Props) => {
 
   const { data: playtime, isLoading } = usePlaytimeQuery(summonerName);
 
-  const playtimeLabel = useMemo(() => {
+  const [maxHoursAllowed, setMaxHoursAllowed] = useLocalStorage<number>({
+    key: localStorageKeys.maxHoursAllowed,
+    defaultValue: 0,
+  });
+
+  const [maxMinutesAllowed, setMaxMinutesAllowed] = useLocalStorage<number>({
+    key: localStorageKeys.maxMinutesAllowed,
+    defaultValue: 0,
+  });
+
+  const [extraHours, setExtraHours] = useLocalStorage<number>({
+    key: localStorageKeys.extraHours,
+    defaultValue: 0,
+  });
+
+  const [extraMinutes, setExtraMinutes] = useLocalStorage<number>({
+    key: localStorageKeys.extraMinutes,
+    defaultValue: 0,
+  });
+
+  const originalPlaytimeLabel = useMemo(() => {
     if (!playtime) return "";
     // 12h 30m
-    const hours = Math.floor(playtime.playedMinutes / 60);
-    const minutes = Math.floor(playtime.playedMinutes % 60);
+
+    const totalMinutes = playtime.playedMinutes;
+
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = Math.floor(totalMinutes % 60);
 
     return `${hours}h ${minutes}m`;
   }, [playtime]);
 
+  const finalPlaytimeLabel = useMemo(() => {
+    if (!playtime) return "";
+    // 12h 30m
+
+    const totalMinutes =
+      playtime.playedMinutes + extraMinutes + extraHours * 60;
+
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = Math.floor(totalMinutes % 60);
+
+    return `${hours}h ${minutes}m`;
+  }, [playtime, extraMinutes]);
+
+  const hasPassedLimit = useMemo(() => {
+    const totalMinutes =
+      (playtime?.playedMinutes || 0) + extraMinutes + extraHours * 60;
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = Math.floor(totalMinutes % 60);
+
+    if (hours > maxHoursAllowed) return true;
+    if (hours === maxHoursAllowed && minutes > maxMinutesAllowed) return true;
+
+    return false;
+  }, [playtime, extraHours, extraMinutes, maxHoursAllowed, maxMinutesAllowed]);
+
+  const [modalOpen, setModalOpen] = useState(false);
+
   if (!summonerName) return null;
 
   return (
-    <Box
-      className="PlaytimePopup"
-      sx={{
-        position: "fixed",
-        bottom: 8,
-        right: 8,
-        bgcolor: "orange",
-        borderRadius: 4,
-        padding: "4px 8px",
-      }}
-    >
-      <FlexCol>
-        <Typography>{summonerName}</Typography>
-        {isLoading && <CircularProgress size={12} />}
+    <>
+      <Box
+        className="PlaytimePopup"
+        sx={{
+          position: "fixed",
+          bottom: 8,
+          right: 8,
+          bgcolor: hasPassedLimit ? "red" : "orange",
+          borderRadius: 4,
+          padding: "4px 8px",
+        }}
+        style={{
+          cursor: "pointer",
+        }}
+        onClick={() => setModalOpen(true)}
+      >
+        <FlexCol>
+          <Typography>{summonerName}</Typography>
+          {isLoading && <CircularProgress size={12} />}
 
-        <span
-          style={{
-            display: "inline-flex",
-            alignItems: "center",
-          }}
-        >
-          {playtimeLabel}
-          <a
-            href={`https://www.op.gg/summoners/br/${summonerName}`}
-            target="_blank"
-            rel="noreferrer"
+          <span>{finalPlaytimeLabel}</span>
+        </FlexCol>
+      </Box>
+
+      <Dialog
+        onClose={() => setModalOpen(false)}
+        open={modalOpen}
+        maxWidth="sm"
+      >
+        <DialogTitle>Time limit</DialogTitle>
+
+        <DialogContent>
+          <FlexVCenter
             style={{
-              marginLeft: 4,
-              bottom: 2,
-              position: "relative",
+              gap: 16,
             }}
           >
-            <CgExternal size={18} />
-          </a>
-        </span>
-      </FlexCol>
-    </Box>
+            <MyTextField
+              label="Max hours"
+              value={maxHoursAllowed}
+              onChange={(e) => {
+                const num = Number(e.target.value);
+                if (isNaN(num)) return;
+                setMaxHoursAllowed(num);
+              }}
+              style={{
+                width: 80,
+              }}
+            />
+
+            <MyTextField
+              label="Max minutes"
+              value={maxMinutesAllowed}
+              onChange={(e) => {
+                const num = Number(e.target.value);
+                if (isNaN(num)) return;
+                setMaxMinutesAllowed(num);
+              }}
+              style={{
+                width: 100,
+              }}
+            />
+          </FlexVCenter>
+
+          <Divider
+            style={{
+              margin: "16px 0",
+            }}
+          />
+          <FlexVCenter
+            style={{
+              gap: 16,
+            }}
+          >
+            <MyTextField
+              label="Extra hours"
+              value={extraHours}
+              onChange={(e) => {
+                const num = Number(e.target.value);
+                if (isNaN(num)) return;
+                setExtraHours(num);
+              }}
+              style={{
+                width: 80,
+              }}
+            />
+
+            <MyTextField
+              label="Extra minutes"
+              value={extraMinutes}
+              onChange={(e) => {
+                const num = Number(e.target.value);
+                if (isNaN(num)) return;
+                setExtraMinutes(num);
+              }}
+              style={{
+                width: 100,
+              }}
+            />
+          </FlexVCenter>
+
+          <FlexVCenter>
+            <Typography
+              style={{
+                marginTop: 16,
+              }}
+            >
+              <a
+                href={`https://www.op.gg/summoners/br/${summonerName}`}
+                target="_blank"
+                rel="noreferrer"
+                style={{
+                  textDecoration: "underline",
+                }}
+              >
+                {originalPlaytimeLabel}
+              </a>{" "}
+              + {extraHours}h {extraMinutes}m = {finalPlaytimeLabel}
+            </Typography>
+          </FlexVCenter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
--- src/utils/consts/localStorageKeys.ts
+++ src/utils/consts/localStorageKeys.ts
@@ -1,4 +1,8 @@
 export const localStorageKeys = {
   editorContent: "editorContent",
   summonerName: "summonerName",
+  maxHoursAllowed: "maxHoursAllowed",
+  maxMinutesAllowed: "maxMinutesAllowed",
+  extraHours: "extraHours",
+  extraMinutes: "extraMinutes",
 };
