857403200268e3366032f5c49166d4c2e83b7180
Paulo Endoh <paulo.endoh@gmail.com>
2023-11-18 14:44:12 -0300

    no need for user

--- src/hooks/react-query/domain/rates/usePlaytimeQuery.ts
+++ src/hooks/react-query/domain/rates/usePlaytimeQuery.ts
@@ -1,11 +1,9 @@
 import myClientAxios from "@/utils/axios/myClientAxios";
 import { urls } from "@/utils/urls/urls";
 import { useQuery } from "react-query";
-import useMeQuery from "../auth/useMeQuery";
 
 export const usePlaytimeQuery = (summonerName: string) => {
   const offsetHours = new Date().getTimezoneOffset() / 60;
-  const { data } = useMeQuery();
 
   return useQuery(
     [urls.api.playtime(offsetHours, summonerName)],
@@ -16,7 +14,7 @@ export const usePlaytimeQuery = (summonerName: string) => {
         }>(urls.api.playtime(offsetHours, summonerName))
         .then((res) => res.data),
     {
-      enabled: !!data && summonerName.length > 0,
+      enabled: summonerName.length > 0,
       staleTime: 1000 * 60 * 2, // 2 minutes
     }
   );
