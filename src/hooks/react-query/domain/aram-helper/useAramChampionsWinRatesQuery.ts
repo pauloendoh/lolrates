5b71b0200286c3fce92281010cb0478447ab983b
Paulo Endoh <paulo.endoh@gmail.com>
2025-08-06 02:47:27 -0300

    aram helper -- MEUS win rates não estão atualizando + mostrar win rate do meu champion quando eu buscar no select [cmdp5uq2o0006zy0lzr4dcrsw]

--- src/hooks/react-query/domain/aram-helper/useAramChampionsWinRatesQuery.ts
+++ src/hooks/react-query/domain/aram-helper/useAramChampionsWinRatesQuery.ts
@@ -1,7 +1,5 @@
 import useAramHelperStore from "@/hooks/zustand-stores/domain/aram-helper/useAramHelperStore";
 import myClientAxios from "@/utils/axios/myClientAxios";
-import { cachePromise } from "@/utils/cachePromise";
-import { localStorageKeys } from "@/utils/consts/localStorageKeys";
 import { queryKeys } from "@/utils/consts/queryKeys";
 import { urls } from "@/utils/urls/urls";
 import { useQuery } from "react-query";
@@ -11,20 +9,12 @@ export const useAramChampionsWinRatesQuery = () => {
   const { summonerName } = useAramHelperStore();
   return useQuery(
     [queryKeys.aramChampionsWinRates(summonerName)],
-    () => {
-      return cachePromise(
-        localStorageKeys.summonerAramChampions(summonerName),
-        1000 * 60 * 60 * 12,
-        () =>
-          myClientAxios
-            .get<AramChampionWinRateDto[]>(
-              urls.api.aramChampionWinRates(summonerName)
-            )
-            .then((res) => {
-              return res.data;
-            })
-      );
-    },
+    () =>
+      myClientAxios
+        .get<AramChampionWinRateDto[]>(
+          urls.api.aramChampionWinRates(summonerName)
+        )
+        .then((res) => res.data),
     {
       enabled: !!summonerName.length,
     }
